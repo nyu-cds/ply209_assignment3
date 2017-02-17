48c6753e4a7cf0c3b57bec17fbcd1e5b1b169508
PatrickYuen <ply209@nyu.edu>
2017-02-17 10:56:02 -0500

    Refactored static BODIES asa global variable and nbody to accept bodies as a local parameter for better modularity

--- nbody_opt.py
+++ nbody_opt.py
@@ -7,7 +7,47 @@
 """
 import itertools
 
-def nbody(loops, reference, iterations):
+#Global Variables
+PI = 3.14159265358979323
+SOLAR_MASS = 4 * PI * PI
+DAYS_PER_YEAR = 365.24
+
+BODIES = {
+	'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+
+	'jupiter': ([4.84143144246472090e+00,
+			-1.16032004402742839e+00,
+			-1.03622044471123109e-01],
+			[1.66007664274403694e-03 * DAYS_PER_YEAR,
+			7.69901118419740425e-03 * DAYS_PER_YEAR,
+			-6.90460016972063023e-05 * DAYS_PER_YEAR],
+			9.54791938424326609e-04 * SOLAR_MASS),
+
+	'saturn': ([8.34336671824457987e+00,
+			4.12479856412430479e+00,
+			-4.03523417114321381e-01],
+			[-2.76742510726862411e-03 * DAYS_PER_YEAR,
+			4.99852801234917238e-03 * DAYS_PER_YEAR,
+			2.30417297573763929e-05 * DAYS_PER_YEAR],
+			2.85885980666130812e-04 * SOLAR_MASS),
+
+	'uranus': ([1.28943695621391310e+01,
+			-1.51111514016986312e+01,
+			-2.23307578892655734e-01],
+			[2.96460137564761618e-03 * DAYS_PER_YEAR,
+			2.37847173959480950e-03 * DAYS_PER_YEAR,
+			-2.96589568540237556e-05 * DAYS_PER_YEAR],
+			4.36624404335156298e-05 * SOLAR_MASS),
+
+	'neptune': ([1.53796971148509165e+01,
+			-2.59193146099879641e+01,
+			1.79258772950371181e-01],
+			[2.68067772490389322e-03 * DAYS_PER_YEAR,
+			1.62824170038242295e-03 * DAYS_PER_YEAR,
+			-9.51592254519715870e-05 * DAYS_PER_YEAR],
+			5.15138902046611451e-05 * SOLAR_MASS)}
+
+def nbody(loops, reference, iterations, bodies):
 	'''
 		nbody simulation
 		loops - number of loops to run
@@ -15,65 +55,25 @@ def nbody(loops, reference, iterations):
 		iterations - number of timesteps to advance
 	'''
 
-	#Local Variables
-	PI = 3.14159265358979323
-	SOLAR_MASS = 4 * PI * PI
-	DAYS_PER_YEAR = 365.24
-
-	BODIES = {
-		'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
-
-		'jupiter': ([4.84143144246472090e+00,
-					 -1.16032004402742839e+00,
-					 -1.03622044471123109e-01],
-					[1.66007664274403694e-03 * DAYS_PER_YEAR,
-					 7.69901118419740425e-03 * DAYS_PER_YEAR,
-					 -6.90460016972063023e-05 * DAYS_PER_YEAR],
-					9.54791938424326609e-04 * SOLAR_MASS),
-
-		'saturn': ([8.34336671824457987e+00,
-					4.12479856412430479e+00,
-					-4.03523417114321381e-01],
-				   [-2.76742510726862411e-03 * DAYS_PER_YEAR,
-					4.99852801234917238e-03 * DAYS_PER_YEAR,
-					2.30417297573763929e-05 * DAYS_PER_YEAR],
-				   2.85885980666130812e-04 * SOLAR_MASS),
-
-		'uranus': ([1.28943695621391310e+01,
-					-1.51111514016986312e+01,
-					-2.23307578892655734e-01],
-				   [2.96460137564761618e-03 * DAYS_PER_YEAR,
-					2.37847173959480950e-03 * DAYS_PER_YEAR,
-					-2.96589568540237556e-05 * DAYS_PER_YEAR],
-				   4.36624404335156298e-05 * SOLAR_MASS),
-
-		'neptune': ([1.53796971148509165e+01,
-					 -2.59193146099879641e+01,
-					 1.79258772950371181e-01],
-					[2.68067772490389322e-03 * DAYS_PER_YEAR,
-					 1.62824170038242295e-03 * DAYS_PER_YEAR,
-					 -9.51592254519715870e-05 * DAYS_PER_YEAR],
-					5.15138902046611451e-05 * SOLAR_MASS)}
-
 	#Offset momentum
 	'''
 		ref is the body in the center of the system
 		offset values from this reference
 	'''
 	(px, py, pz) = (0.0, 0.0, 0.0)
-	for body in BODIES.keys():
-		(_, [vx, vy, vz], m) = BODIES[body]
+	for body in bodies.keys():
+		(_, [vx, vy, vz], m) = bodies[body]
 		px -= vx * m
 		py -= vy * m
 		pz -= vz * m
 
-	(r, v, m) = BODIES[reference]
+	(r, v, m) = bodies[reference]
 	v[0] = px / m
 	v[1] = py / m
 	v[2] = pz / m
 
 	# Set up body pairs
-	body_pairs = list(itertools.combinations(BODIES.keys(), 2))
+	body_pairs = list(itertools.combinations(bodies.keys(), 2))
 
 	for i in range(loops * iterations):
 		'''
@@ -83,8 +83,8 @@ def nbody(loops, reference, iterations):
 		dt = 0.01
 
 		for (body1, body2) in body_pairs:
-			([x1, y1, z1], v1, m1) = BODIES[body1]
-			([x2, y2, z2], v2, m2) = BODIES[body2]
+			([x1, y1, z1], v1, m1) = bodies[body1]
+			([x2, y2, z2], v2, m2) = bodies[body2]
 
 			# Compute Deltas
 			(dx, dy, dz) = (x1-x2, y1-y2, z1-z2)
@@ -99,8 +99,8 @@ def nbody(loops, reference, iterations):
 			v2[1] += dy * m1 * mag
 			v2[2] += dz * m1 * mag
 
-		for body in BODIES.keys():
-			(r, [vx, vy, vz], m) = BODIES[body]
+		for body in bodies.keys():
+			(r, [vx, vy, vz], m) = bodies[body]
 
 			#update_rs(r, dt, vx, vy, vz)
 			r[0] += dt * vx
@@ -114,8 +114,8 @@ def nbody(loops, reference, iterations):
 				compute the energy and return it so that it can be printed
 			'''
 			for (body1, body2) in body_pairs:
-				((x1, y1, z1), v1, m1) = BODIES[body1]
-				((x2, y2, z2), v2, m2) = BODIES[body2]
+				((x1, y1, z1), v1, m1) = bodies[body1]
+				((x2, y2, z2), v2, m2) = bodies[body2]
 
 				# Input Deltas
 				(dx, dy, dz) = (x1 - x2, y1 - y2, z1 - z2)
@@ -123,8 +123,8 @@ def nbody(loops, reference, iterations):
 				# Compute energy
 				report_energy -= (m1 * m2) / ((dx ** 2 + dy ** 2 + dz ** 2) ** 0.5)
 
-			for body in BODIES.keys():
-				(r, [vx, vy, vz], m) = BODIES[body]
+			for body in bodies.keys():
+				(r, [vx, vy, vz], m) = bodies[body]
 				report_energy += m * (vx * vx + vy * vy + vz * vz) / 2.
 
 			print(report_energy)
@@ -134,6 +134,4 @@ if __name__ == '__main__':
 	import timeit
 
 	#Timing It
-	print(timeit.timeit("nbody(100, 'sun', 20000)", setup="from __main__ import nbody", number=1))
-
-
+	print(timeit.timeit("nbody(100, 'sun', 20000, BODIES)", setup="from __main__ import BODIES,nbody", number=1))
